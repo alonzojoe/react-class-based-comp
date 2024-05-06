3bfc6245565d8e896b15d72428ce04fe8db04b7c
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-06 14:39:30 +0800

    Managing state and events in class based components

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,36 +1,51 @@
-import { useState } from "react";
+import { Component, useState } from "react";
 import User from "./User";
 import classes from "./Users.module.css";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
-
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
-
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? "Hide" : "Show"} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
+const generateRandomID = () => {
+  return Math.random().toString().slice(2, 7);
 };
+
+const DUMMY_USERS = [
+  { id: generateRandomID(), name: "Joe" },
+  { id: generateRandomID(), name: "John" },
+  { id: generateRandomID(), name: "Jane" },
+];
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+      data: [],
+    };
+  }
+
+  toggleUsersHandler() {
+    this.setState((prevState) => {
+      return {
+        showUsers: !prevState.showUsers,
+      };
+    });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
 
 export default Users;
